c6c72ae668e1bf09b327b4266691441a423238a5
ibrahimmbyrrm <ibrahim_bayram22@yahoo.com>
2023-03-01 19:23:28 +0300

    Initial Commit

---
+++ Prayer-Time-ReminderUITests/Prayer_Time_ReminderUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Prayer_Time_ReminderUITestsLaunchTests.swift
+//  Prayer-Time-ReminderUITests
+//
+//  Created by İbrahim Bayram on 1.03.2023.
+//
+
+import XCTest
+
+final class Prayer_Time_ReminderUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
